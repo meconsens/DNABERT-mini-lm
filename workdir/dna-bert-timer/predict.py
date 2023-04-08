4b0b9a426c38f2f4201816c608878dcbc4a6acdc
matthew9655 <matthew9655@hotmail.com>
2023-04-08 18:04:48 -0400

    add timer code

---
+++ workdir/dna-bert-timer/predict.py
@@ -0,0 +1,43 @@
+import torch
+import csv
+from transformers import DistilBertForSequenceClassification
+from transformers import AutoTokenizer
+import sys
+# import os
+
+# read cmdline parameters & do error checking
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+#load the model
+model = DistilBertForSequenceClassification.from_pretrained('model/')
+model.eval()
+tokenizer = AutoTokenizer.from_pretrained('model/')
+
+#create classification label
+classifications = ["PROXIMAL", "CORE"]
+
+#load the user input
+file = open((input_path), "r")
+reader = csv.reader(file, delimiter=',')
+predictions = []
+sequence_count = 0
+for row in reader:
+    if sequence_count < 100:
+        encoding = tokenizer.encode(row[0], return_tensors='pt')
+        input_ids = encoding 
+        attention_mask = torch.ones(input_ids.shape)
+        output = model(input_ids=input_ids, attention_mask=attention_mask)[0]
+        output = torch.nn.functional.softmax(model(input_ids=input_ids, attention_mask=attention_mask)[0], dim=-1)
+        classification = output.argmax(dim=-1).item()
+        class_name = classifications[classification]
+        predictions.append((f"This might be an input sequence from a {class_name} promoter region.\n"))
+        sequence_count=sequence_count+1
+    else:
+        print('sequence_count < 100 ')
+        exit
+    
+with open(output_path, 'w') as f:
+        f.writelines(predictions)
+
+
--- workdir/docker_test.sh
+++ workdir/docker_test.sh
@@ -1,4 +1,8 @@
-docker run --rm --name dna-bert-tester \
--v $(pwd)/dna-bert/test_workdir:/parcel \
-  test1124 \
-  /parcel/data/in/sequence_sample.csv /parcel/data/out/prediction.txt
+docker run --rm --network host ping
+  # /parcel/data/in/sequence_sample_1.csv /parcel/data/in/sequence_sample_5.csv \
+  # /parcel/data/in/sequence_sample_20.csv /parcel/data/in/sequence_sample_100.csv \
+  # /parcel/data/out/prediction.txt
+
+  # test_workdir/data/in/sequence_sample_1.csv test_workdir/data/in/sequence_sample_5.csv \
+  # test_workdir/data/in/sequence_sample_20.csv test_workdir/data/in/sequence_sample_100.csv \
+  # test_workdir/data/out/prediction.txt
